4ef88947f8c857870f44e5272cd3a676a287e0d8
miwanoff <marina.iva.noffa@gmail.com>
2023-10-15 12:49:14 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const str1 =
+  "Lorem Spam ipsum dolor sit amet consectetur adipisicing elit. Tempora id commodi neque, unde amet adipisci eius molestias error, quae cumque placeat quidem illum dolorem tenetur porro molestiae quas officia culpa.";
+
+const str2 =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora id commodi neque, unde amet adipisci eius molestias error, quae cumque placeat quidem illum dolorem tenetur porro molestiae quas officia culpa.";
+
+function check(str) {
+  let spam = "spam";
+  console.log(str.trim().toLowerCase());
+  
+}
+
+check(str1);
