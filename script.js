77b8aa5ecbddaef9a042bab1fbabd91de57e7385
miwanoff <marina.iva.noffa@gmail.com>
2023-10-15 12:57:06 +0300

    Add todo parameter

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const str2 =
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora id commodi neque, unde amet adipisci eius molestias error, quae cumque placeat quidem illum dolorem tenetur porro molestiae quas officia culpa.";
 
 function check(str) {
-  let spam = "spam";
+  //let spam = "spam"; // Передавати як параметр
   console.log(str.trim().toLowerCase());
-  
+
 }
 
 check(str1);
