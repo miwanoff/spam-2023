da4216fbe44def17859588b0292d63bff1842b84
miwanoff <marina.iva.noffa@gmail.com>
2023-10-21 12:23:10 +0300

    Add simple variant hints

---
+++ palindrom.js
@@ -0,0 +1,33 @@
+let palindroms = [
+    "А роза упала на лапу Азора",
+    "Ого",
+    "А Собака боса",
+    "Привет, мир",
+    "А Собака боса",
+    "Лёша на полке клопа нашёл",
+    "Я — арка края",
+  ];
+
+  function checkPalindrom(arr) {
+    let pals =[];
+    for (let i = 0; i < arr.length; i++) {
+        let str = arr[i].toLowerCase();
+        str = str.replaceAll(" ", "");
+        str = str.replaceAll("—", "");
+        str = str.replaceAll(",", "");
+        console.log(str);
+        //console.log(arr[i]);
+        // Todo reverse and copmare
+        //let str1 = str.split("");
+        reversed = reversed.reverse();
+        reversed = reversed.join("");
+        console.log(reversed);
+        if (str == reversed){
+          pals.
+        }
+        //pals[i] = reversed;
+    }
+    return pals;
+  }
+
+  console.log(checkPalindrom(palindroms));
---
+++ preview.js
@@ -0,0 +1,10 @@
+const str1 = "Lorem ipsum dolor sit";
+const str2 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora id commodi neque, unde amet adipisci eius molestias error, quae cumque placeat quidem illum dolorem tenetur porro molestiae quas officia culpa.";
+
+function preview(str) {
+   // let n = 20;
+    if (str.length >= n) {
+        str.substr(0, n);
+        // lastIndexOf(" ")
+    }
+}
